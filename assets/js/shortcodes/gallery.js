631b0a3d243393f9af39394f3bbbd1f89b093dd2
StandingPad Animations <mahidsheikh@pm.me>
2024-04-15 17:50:44 -0500

    Changed vendor and gallary script loading

--- assets/js/shortcodes/gallery.js
+++ assets/js/shortcodes/gallery.js
@@ -1,20 +1,18 @@
-(function init() {
-    htmx.onLoad(function(content) {
-        console.groupCollapsed('[DEBUG] Gallery feature enable');
-        let magicGrids = [];
-        let nodeGalleries = content.querySelectorAll('.gallery');
+htmx.onLoad(function(content) {
+    console.groupCollapsed('[DEBUG] Gallery feature enable');
+    let magicGrids = [];
+    let nodeGalleries = content.querySelectorAll('.gallery');
 
-        nodeGalleries.forEach(nodeGallery => {
-            let magicGrid = new MagicGrid({
-                container: nodeGallery,  
-                static: true,
-                gutter: 5,
-                animate: false,
-            });
-            magicGrid.listen();
-            magicGrids.push(magicGrid);
+    nodeGalleries.forEach(nodeGallery => {
+        let magicGrid = new MagicGrid({
+            container: nodeGallery,  
+            static: true,
+            gutter: 5,
+            animate: false,
         });
-        console.log("Galleries founded and initialized with MagicGrid", magicGrids);
-        console.groupEnd();
+        magicGrid.listen();
+        magicGrids.push(magicGrid);
     });
-})();
+    console.log("Galleries founded and initialized with MagicGrid", magicGrids);
+    console.groupEnd();
+});
