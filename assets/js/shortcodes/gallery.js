37384bb646d50a8bcd7f2a60e5d1d6065ecbe2d9
StandingPad Animations <mahidsheikh@pm.me>
2024-04-15 18:25:55 -0500

    Updated gallery.js

--- assets/js/shortcodes/gallery.js
+++ assets/js/shortcodes/gallery.js
@@ -1,18 +1,20 @@
 htmx.onLoad(function(content) {
-    console.groupCollapsed('[DEBUG] Gallery feature enable');
-    let magicGrids = [];
-    let nodeGalleries = content.querySelectorAll('.gallery');
+    $(content).ready(function () {
+        console.groupCollapsed('[DEBUG] Gallery feature enable');
+        let magicGrids = [];
+        let nodeGalleries = document.querySelectorAll('.gallery');
 
-    nodeGalleries.forEach(nodeGallery => {
-        let magicGrid = new MagicGrid({
-            container: nodeGallery,  
-            static: true,
-            gutter: 5,
-            animate: false,
+        nodeGalleries.forEach(nodeGallery => {
+            let magicGrid = new MagicGrid({
+                container: nodeGallery,  
+                static: true,
+                gutter: 5,
+                animate: false,
+            });
+            magicGrid.listen();
+            magicGrids.push(magicGrid);
         });
-        magicGrid.listen();
-        magicGrids.push(magicGrid);
+        console.log("Galleries founded and initialized with MagicGrid", magicGrids);
+        console.groupEnd();
     });
-    console.log("Galleries founded and initialized with MagicGrid", magicGrids);
-    console.groupEnd();
 });
